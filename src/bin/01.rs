5940e92559ffc9ab695f88004f6e17216e4147b6
steveklabnik <steve@steveklabnik.com>
2016-12-01 22:30:05 -0500

    do some refactoring
    * move errors to a lib
    * move cache out to its own thing

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,8 +1,8 @@
-mod error;
+extern crate latent_catholicism;
 
 use std::str::FromStr;
 use std::collections::HashSet;
-use error::ParseError;
+use latent_catholicism::one::ParseError;
 
 #[derive(PartialEq, Debug)]
 enum Direction {
@@ -60,6 +60,60 @@ impl FromStr for Instructions {
     }
 }
 
+struct PositionCache {
+    cache: HashSet<(i64, i64)>,
+    duplicate: Option<(i64, i64)>,
+}
+
+impl PositionCache {
+    fn new() -> PositionCache {
+        PositionCache {
+            cache: HashSet::new(),
+            duplicate: None,
+        }
+    }
+
+    fn update_x(&mut self, position: (i64, i64), distance: i64) {
+        for count in 0..distance.abs() {
+            let seen_position = if distance > 0 {
+                (position.0 + count, position.1)
+            } else {
+                (position.0 - count, position.1)
+            };
+
+            println!("visiting: {:?}", seen_position);
+
+            if self.duplicate.is_none() {
+                if self.cache.contains(&seen_position) {
+                    self.duplicate = Some(seen_position);
+                } else {
+                    self.cache.insert(seen_position);
+                }
+            }
+        }
+    }
+
+    fn update_y(&mut self, position: (i64, i64), distance: i64) {
+        for count in 0..distance.abs() {
+            let seen_position = if distance > 0 {
+                (position.0, position.1 + count)
+            } else {
+                (position.0, position.1 - count)
+            };
+
+            println!("visiting: {:?}", seen_position);
+
+            if self.duplicate.is_none() {
+                if self.cache.contains(&seen_position) {
+                    self.duplicate = Some(seen_position);
+                } else {
+                    self.cache.insert(seen_position);
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     let input = "L1, L3, L5, L3, R1, L4, L5, R1, R3, L5, R1, L3, L2, L3, R2, R2, L3, L3, R1, L2, R1, L3, L2, R4, R2, L5, R4, L5, R4, L2, R3, L2, R4, R1, L5, L4, R1, L2, R3, R1, R2, L4, R1, L2, R3, L2, L3, R5, L192, R4, L5, R4, L1, R4, L4, R2, L5, R45, L2, L5, R4, R5, L3, R5, R77, R2, R5, L5, R1, R4, L4, L4, R2, L4, L1, R191, R1, L1, L2, L2, L4, L3, R1, L3, R1, R5, R3, L1, L4, L2, L3, L1, L1, R5, L4, R1, L3, R1, L2, R1, R4, R5, L4, L2, R4, R5, L1, L2, R3, L4, R2, R2, R3, L2, L3, L5, R3, R1, L4, L3, R4, R2, R2, R2, R1, L4, R4, R1, R2, R1, L2, L2, R4, L1, L2, R3, L3, L5, L4, R4, L3, L1, L5, L3, L5, R5, L5, L4, L2, R1, L2, L4, L2, L4, L1, R4, R4, R5, R1, L4, R2, L4, L2, L4, R2, L4, L1, L2, R1, R4, R3, R2, R2, R5, L1, L2";
 
@@ -71,144 +125,79 @@ fn main() {
 
     let mut position = (0i64, 0i64);
     let mut direction = Direction::Up;
-    let mut seen = HashSet::new();
-    let mut seen_twice = None;
+    let mut cache = PositionCache::new();
 
     for instruction in directions.0 {
         direction = match (instruction.direction, direction) {
             (InstructionDirection::Left, Direction::Left) => {
-                for count in 0..instruction.distance {
-                    let seen_position = (position.0, position.1 - count as i64);
+                let distance = instruction.distance as i64;
 
-                    if seen_twice.is_none() {
-                        if seen.contains(&seen_position) {
-                            seen_twice = Some(seen_position);
-                        } else {
-                            seen.insert(seen_position);
-                        }
-                    }
-                }
+                cache.update_y(position, -distance);
 
-                position.1 -= instruction.distance as i64;
+                position.1 -= distance;
 
                 Direction::Down
             },
             (InstructionDirection::Left, Direction::Right) => {
-                for count in 0..instruction.distance {
-                    let seen_position = (position.0, position.1 + count as i64);
+                let distance = instruction.distance as i64;
 
-                    if seen_twice.is_none() {
-                        if seen.contains(&seen_position) {
-                            seen_twice = Some(seen_position);
-                        } else {
-                            seen.insert(seen_position);
-                        }
-                    }
-                }
+                cache.update_y(position, distance);
 
-                position.1 += instruction.distance as i64;
+                position.1 += distance;
 
                 Direction::Up
             },
             (InstructionDirection::Left, Direction::Up) => {
-                for count in 0..instruction.distance {
-                    let seen_position = (position.0 - count as i64, position.1);
+                let distance = instruction.distance as i64;
 
-                    if seen_twice.is_none() {
-                        if seen.contains(&seen_position) {
-                            seen_twice = Some(seen_position);
-                        } else {
-                            seen.insert(seen_position);
-                        }
-                    }
-                }
+                cache.update_x(position, -distance);
 
-                position.0 -= instruction.distance as i64;
+                position.0 -= distance;
 
                 Direction::Left
             },
             (InstructionDirection::Left, Direction::Down) => {
-                for count in 0..instruction.distance {
-                    let seen_position = (position.0 + count as i64, position.1);
+                let distance = instruction.distance as i64;
 
-                    if seen_twice.is_none() {
-                        if seen.contains(&seen_position) {
-                            seen_twice = Some(seen_position);
-                        } else {
-                            seen.insert(seen_position);
-                        }
-                    }
-                }
+                cache.update_x(position, distance);
 
-                position.0 += instruction.distance as i64;
+                position.0 += distance;
 
                 Direction::Right
             },
             (InstructionDirection::Right, Direction::Left) => {
-                for count in 0..instruction.distance {
-                    let seen_position = (position.0, position.1 + count as i64);
+                let distance = instruction.distance as i64;
 
-                    if seen_twice.is_none() {
-                        if seen.contains(&seen_position) {
-                            seen_twice = Some(seen_position);
-                        } else {
-                            seen.insert(seen_position);
-                        }
-                    }
-                }
+                cache.update_y(position, distance);
 
-                position.1 += instruction.distance as i64;
+                position.1 += distance;
 
                 Direction::Up
             },
             (InstructionDirection::Right, Direction::Right) => {
-                for count in 0..instruction.distance {
-                    let seen_position = (position.0, position.1 - count as i64);
+                let distance = instruction.distance as i64;
 
-                    if seen_twice.is_none() {
-                        if seen.contains(&seen_position) {
-                            seen_twice = Some(seen_position);
-                        } else {
-                            seen.insert(seen_position);
-                        }
-                    }
-                }
+                cache.update_y(position, -distance);
 
-                position.1 -= instruction.distance as i64;
+                position.1 -= distance;
 
                 Direction::Down
             },
             (InstructionDirection::Right, Direction::Up) => {
-                for count in 0..instruction.distance {
-                    let seen_position = (position.0 + count as i64, position.1);
+                let distance = instruction.distance as i64;
 
-                    if seen_twice.is_none() {
-                        if seen.contains(&seen_position) {
-                            seen_twice = Some(seen_position);
-                        } else {
-                            seen.insert(seen_position);
-                        }
-                    }
-                }
+                cache.update_x(position, distance);
 
-                position.0 += instruction.distance as i64;
+                position.0 += distance;
 
                 Direction::Right
             },
             (InstructionDirection::Right, Direction::Down) => {
-                for count in 0..instruction.distance {
-                    let seen_position = (position.0 - count as i64, position.1);
+                let distance = instruction.distance as i64;
 
-                    if seen_twice.is_none() {
-                        if seen.contains(&seen_position) {
-                            seen_twice = Some(seen_position);
-                        } else {
-                            seen.insert(seen_position);
-                        }
-                    }
-                }
+                cache.update_x(position, -distance);
 
-                position.0 -= instruction.distance as i64;
+                position.0 -= distance;
 
                 Direction::Left
             },
@@ -218,7 +207,7 @@ fn main() {
     println!("end spot: ({},{})", position.0, position.1);
     println!("blocks: {}", position.0.abs() + position.1.abs());
 
-    if let Some(seen) = seen_twice {
+    if let Some(seen) = cache.duplicate {
         println!("seen twice: {:?}", seen);
         println!("blocks: {}", seen.0.abs() + seen.1.abs())
     }
